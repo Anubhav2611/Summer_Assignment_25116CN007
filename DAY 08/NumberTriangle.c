f6a408af9852796c15d356995f83e20c4ea1f2a2
Anubhav2611 <anubhav.kushinagar@gmail.com>
2026-06-05 17:40:48 +0530

    Add files via upload

---
+++ DAY 08/NumberTriangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("input n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int m=1;
+        for(int j=1;j<=i;j++){
+            printf("%d",m);
+            m++;
+        }   printf("\n");
+        
+    } return 0;
+    
+}
---
+++ DAY 08/RepeatedNumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,m=1;
+    printf("input n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",m);
+        }printf("\n");
+        m++;
+    }return 0;
+}
